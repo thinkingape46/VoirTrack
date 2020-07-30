f7411df4d5961549d039776685a6de5fcc04afb8
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-07-30 17:53:01 +0530

    initial setup

---
+++ app/assets/scripts/app.js
@@ -0,0 +1,12 @@
+var map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([77.7184446, 12.9655114]),
+      zoom: 8
+    })
+  });
