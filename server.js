383f7454007e29a72b8320e27faea1137bf41870
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-08-21 16:06:19 +0530

    procfile added

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+};
+
 app.use(express.static('public'));
 app.set('views', 'views');
 app.set('view engine', 'ejs');
@@ -9,4 +14,4 @@ app.get('/', function(req, res) {
     res.render('index.ejs');
 })
 
-app.listen(3000);
+app.listen(port);
