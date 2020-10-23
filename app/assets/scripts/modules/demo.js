4a0c2d6a767fc27f8e4adf78311531fca900cc48
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-10-23 23:46:07 +0530

    successfully render gpx on map

--- app/assets/scripts/modules/demo.js
+++ app/assets/scripts/modules/demo.js
@@ -6,11 +6,9 @@ class Demo {
         this.demoButton = document.getElementById("demo");
         this.events();
     }
-
     events() {
         this.demoButton.addEventListener("click", (e) => this.parseGpx(e));
     }
-
     renderGPX(e) {
         e.preventDefault();
         let filePath = '/assets/tracks/run-sample.gpx';
@@ -20,18 +18,34 @@ class Demo {
             simpleMap.fitBounds(e.target.getBounds())
         }).addTo(simpleMap);
     }
-
     parseGpx(e) {
         e.preventDefault();
-
-        let fileUrl = "";
+        let fileUrl = "https://raw.githubusercontent.com/thinkingape46/VoirTrack/master/public/assets/tracks/short-ride.gpx";
         let xhr = new XMLHttpRequest();
         xhr.open('GET', fileUrl);
         xhr.responseType = XMLDocument;
-        xhr.onload = (e) => this.renderDemo(e);
+        xhr.onload = (e) => {
+            this.renderDemo(xhr.response);
+        }
+        xhr.send();
     }
-    renderDemo(e) {
-        
+    renderDemo(parsedGpx) {
+        console.log(parsedGpx);
+        let domParser = new DOMParser();
+        let gpxParsedDocument = domParser.parseFromString(parsedGpx, "text/xml");
+        let trackPoints = gpxParsedDocument.getElementsByTagName("trkpt");
+
+        let i;
+        let latLangs = [];
+
+        for (i = 0; i < trackPoints.length; i++) {
+            let lat = parseFloat(trackPoints[i].attributes[0].value);
+            let lang = parseFloat(trackPoints[i].attributes[1].value);
+
+            latLangs.push([lat, lang]);            
+        }
+
+        L.polyline(latLangs, {color: 'red'}).addTo(simpleMap);
     }
 }
 
