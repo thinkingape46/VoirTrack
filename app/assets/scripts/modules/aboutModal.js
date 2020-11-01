bcd5506970662d790c24ff95b192c373a7134ae7
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-11-01 22:30:25 +0530

    about paged modified: 1. button image changed, 2. layout changed (font, positioning, font-size), takes the whole screen now. 3. Description also changed during the last commit

--- app/assets/scripts/modules/aboutModal.js
+++ app/assets/scripts/modules/aboutModal.js
@@ -24,26 +24,26 @@ class AboutModal {
 
                 <div class="about__container__content">
                     <h1 class="text-content about__container__content__title">What is this GPX Chemin for?</h1>
-                    <main class="about__container__content__text">
-                        <ul class="text-content about__container__content__text--item">
-                            <li>GPX Chemin is a simple web-app to visualize GPX files on a map.</li>
+                    <main class="about__container__main">
+                        <ul class="text-content about__container__ul">
+                            <li class="about__container__content__text__item">GPX Chemin is a simple web-app to visualize GPX files on a map.</li>
 
-                            <li>Files can be added by dropping files over <strong>Drop files here</strong> area or by clicking on <strong>Add files</strong> to select from file manager.</li>
+                            <li class="about__container__content__text__item">Files can be added by dropping files over <strong>Drop files here</strong> area or by clicking on <strong>Add files</strong> to select from file manager.</li>
 
-                            <li>If files are added by using file browser, click on <strong>Plot</strong> to load the track on the map. No action is needed when files are dropped over drop area, tracks get loaded automatically.</li>
+                            <li class="about__container__content__text__item">If files are added by using file browser, click on <strong>Plot</strong> to load the track on the map. No action is needed when files are dropped over drop area, tracks get loaded automatically.</li>
 
-                            <li>Single or multiple files can be added at once, or can be added progressively.</li>
+                            <li class="about__container__content__text__item">Single or multiple files can be added at once, or can be added progressively.</li>
 
-                            <li>On the right side, all the basic stats of each track such as title, date, distance, speed, elevation, and heart rate are displayed.</li>
+                            <li class="about__container__content__text__item">On the right side, all the basic stats of each track such as title, date, distance, speed, elevation, and heart rate are displayed.</li>
 
-                            <li>The color of each stat box matches with the color of the respective line on the map.</li>
+                            <li class="about__container__content__text__item">The color of each stat box matches with the color of the respective line on the map.</li>
                             
-                            <li>Under the map are three graphs, one each for speed (blue), elevation (teal), and heart rate (red).</li>
+                            <li class="about__container__content__text__item">Under the map are three graphs, one each for speed (blue), elevation (teal), and heart rate (red).</li>
 
-                            <li>Stats and graphs are displayed only when a track is loaded, if you don't have any gpx file with you, click on <strong>Test</strong> to load a demo track.</li>
+                            <li class="about__container__content__text__item">Stats and graphs are displayed only when a track is loaded, if you don't have any gpx file with you, click on <strong>Test</strong> to load a demo track.</li>
 
                             <br>
-                            <li>Enter escape key or click on close button to exit this window.</li>
+                            <li class="about__container__content__text__item">Enter escape key or click on close button to exit this window.</li>
                         </ul>
                     </main>
                 </div>
