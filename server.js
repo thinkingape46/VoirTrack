f2f55ec5f5771fc5ae0321f5d07eb5c454cda5cb
jagadeesh <jagadeesh.kumar2091@gmail.com>
2021-04-04 12:10:16 +0530

    Removed unused webpack config

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const app = express();
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-    port = 3000;
-};
+  port = 3000;
+}
 
-app.use(express.static('dist'));
-app.use(express.static('public'));
-app.set('views', 'views');
-app.set('view engine', 'ejs');
+app.use(express.static("dist"));
+app.use(express.static("public"));
+app.set("views", "views");
+app.set("view engine", "ejs");
 
-app.get('/', function(req, res) {
-    res.render('index.ejs');
-})
+app.get("/", function (req, res) {
+  res.render("index.ejs");
+});
 
 app.listen(port);
